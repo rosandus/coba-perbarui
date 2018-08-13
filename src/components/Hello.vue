5ae2a9a2efd9a1844a0f91991040b4b9765b2c42
Rosa Nur Rizky Febrianty Tamarindus <rosatamarindus@gmail.com>
2018-08-13 12:21:01 +0700

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+     <vue-status>
+     <h2 slot="offline">You are offline</h2>
+     <h2 slot="online">You are online (Now go to network tab and go offline)</h2>
+ </vue-status>
+  </div>
+</template>
+
+<script>
+import VueStatus from "vue-status";
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome!'
+    }
+  }, components : {
+    VueStatus
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
